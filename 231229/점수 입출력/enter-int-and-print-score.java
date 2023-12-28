fd9e588fbe15d6f301e9ee31d5a0fecd1d2a92a2
Chaerin <150896440+chaexrin@users.noreply.github.com>
2023-12-29 01:05:01 +0900

    Title: 점수 입출력, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231229/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        System.out.printf("Your score is %d point.", n);
+    }
+}
