3c0cb983b71907fe6e93fce4e95cdbf185742727
Chaerin <150896440+chaexrin@users.noreply.github.com>
2023-12-29 18:06:16 +0900

    Title: 실수 입력받아 계산, Time: 153ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 231229/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+       java.util.Scanner sc = new java.util.Scanner(System.in);
+
+       double a = sc.nextDouble();
+       double b = sc.nextDouble();
+
+       System.out.printf("%.2f", a+b);
+    }
+}
