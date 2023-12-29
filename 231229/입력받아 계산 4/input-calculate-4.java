ab282a6b3eacf14a849000ff8c7ec718515dbedd
Chaerin <150896440+chaexrin@users.noreply.github.com>
2023-12-29 09:27:35 +0900

    Title: 입력받아 계산 4, Time: 162ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231229/입력받아 계산 4/input-calculate-4.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a * 2);
+    }
+}
