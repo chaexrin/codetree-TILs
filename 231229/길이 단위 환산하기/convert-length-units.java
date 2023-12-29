cf54538969c96ac217028bfe782702dc04242fbf
Chaerin <150896440+chaexrin@users.noreply.github.com>
2023-12-29 09:50:33 +0900

    Title: 길이 단위 환산하기, Time: 252ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 231229/길이 단위 환산하기/convert-length-units.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        double n = sc.nextDouble();
+
+        System.out.printf("%.1f", n*30.48);
+    }
+}
