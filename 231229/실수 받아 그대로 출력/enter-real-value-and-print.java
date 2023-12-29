1a9a3aa05fcfc67a09523a1c6ed84ab44c27696d
Chaerin <150896440+chaexrin@users.noreply.github.com>
2023-12-29 09:40:53 +0900

    Title: 실수 받아 그대로 출력, Time: 178ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 231229/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        double n = sc.nextDouble();
+        System.out.printf("%.2f", n);
+    }
+}
