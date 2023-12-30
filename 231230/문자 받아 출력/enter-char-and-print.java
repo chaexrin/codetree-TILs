bb0f41b227de9e4ebe8bd52b833a511104970de2
Chaerin <150896440+chaexrin@users.noreply.github.com>
2023-12-30 19:33:59 +0900

    Title: 문자 받아 출력, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231230/문자 받아 출력/enter-char-and-print.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        String s = sc.next();
+        System.out.println(s);
+    }
+}
