4d7bfa97f726bd40a2497103085ee88844afa6fa
Chaerin <150896440+chaexrin@users.noreply.github.com>
2023-12-30 21:26:26 +0900

    Title: 날짜 변경하여 출력 2, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231230/날짜 변경하여 출력 2/print-date-with-different-format-2.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+sc.useDelimiter("-");
+int mm = sc.nextInt();
+int dd = sc.nextInt();
+int yyyy = sc.nextInt();
+
+System.out.println(yyyy+"."+mm+"."+dd);
+
+    }
+}
