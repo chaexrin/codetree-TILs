0c709050869bb65ea4ada53344f0ba98606c81cf
Chaerin <150896440+chaexrin@users.noreply.github.com>
2023-12-31 17:22:38 +0900

    Title: 전화번호 바꾸기, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231231/전화번호 바꾸기/change-phone-number.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        sc.useDelimiter("-");
+        int a = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        System.out.println("0"+a+"-"+y+"-"+x);
+    }
+}
