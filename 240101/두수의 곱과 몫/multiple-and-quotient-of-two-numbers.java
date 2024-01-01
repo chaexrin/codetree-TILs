270865bd2cd128928ece3666fa67d32cc71e3db7
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-01-01 18:28:08 +0900

    Title: 두수의 곱과 몫, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240101/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + " * " + b +" = "+(a*b));
+        System.out.println("300 / 5 = " + (a / b));
+    }
+}
