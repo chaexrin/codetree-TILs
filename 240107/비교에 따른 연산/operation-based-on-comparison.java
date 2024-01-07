52c50a4babfb888d62e0cd10581f3045420f69db
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-01-07 22:32:21 +0900

    Title: 비교에 따른 연산, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240107/비교에 따른 연산/operation-based-on-comparison.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+       java.util.Scanner sc = new java.util.Scanner(System.in);
+
+       int a = sc.nextInt();
+       int b = sc.nextInt();
+
+       if ( a > b) {
+        System.out.println(a*b);
+       } else {
+        System.out.println(a/b);
+       }
+    }
+}
