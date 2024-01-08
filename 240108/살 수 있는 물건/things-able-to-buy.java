edad6f68a958ff2886dcd9547beda34e2e4ac9cf
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-01-08 21:43:58 +0900

    Title: 살 수 있는 물건, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240108/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if ( n >= 3000)
+        System.out.println("book");
+        else if ( n >= 1000) 
+        System.out.println("mask");
+        else 
+        System.out.println("no");
+    }
+}
