9dd9c8cb09b0f673bd49ec2e8c96865c1dbd0b9f
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-01-09 14:32:13 +0900

    Title: 출석 부르기, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240109/출석 부르기/calling-attendance.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+    java.util.Scanner sc = new java.util.Scanner(System.in);
+
+    int a = sc.nextInt();
+
+    if ( a == 1)
+    System.out.println("John");
+    else if ( a == 2)
+    System.out.println("Tom");
+    else if ( a == 3)
+    System.out.println("Paul");
+    else 
+    System.out.println("Vacancy");
+
+    }
+}
