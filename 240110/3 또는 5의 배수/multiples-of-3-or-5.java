2fa7b9f3b20aa1b56fb63ed57b84ed69e5df29fa
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-01-10 20:55:02 +0900

    Title: 3 또는 5의 배수, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240110/3 또는 5의 배수/multiples-of-3-or-5.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+       java.util.Scanner sc = new java.util.Scanner(System.in);
+
+       int a = sc.nextInt();
+
+       if (a %3 == 0) {
+        System.out.println("YES");
+       } else {
+        System.out.println("NO");
+       }
+
+              if (a %5 == 0) {
+        System.out.println("YES");
+       } else {
+        System.out.println("NO");
+       }
+    }
+}
