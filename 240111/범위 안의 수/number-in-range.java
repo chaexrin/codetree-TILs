c4357c3cba75ecb48920a8bf5216ff1ce24eb57e
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-01-11 12:04:16 +0900

    Title: 범위 안의 수, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240111/범위 안의 수/number-in-range.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        if (a >= 10 && a <= 20)
+        System.out.println("yes");
+        else
+        System.out.println("no");
+    }
+}
