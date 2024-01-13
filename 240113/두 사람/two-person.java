171946025452f4e1651674e80129f58416af6cca
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-01-13 20:42:26 +0900

    Title: 두 사람, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240113/두 사람/two-person.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        int a_age = sc.nextInt();
+        String a_sex = sc.next();
+         int b_age = sc.nextInt();
+        String b_sex = sc.next();
+        
+        if ((a_age >= 19 && a_sex.equals("M")) || (b_age >= 19 && b_sex.equals("M"))) {
+            System.out.println("1");
+        } else {
+        System.out.println("0");
+        }
+    }
+}
