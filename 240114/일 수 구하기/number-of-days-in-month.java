7fcfc0edbc9fec0b1bce7e0ed1a99fc068c7fa36
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-01-14 16:42:43 +0900

    Title: 일 수 구하기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240114/일 수 구하기/number-of-days-in-month.java
+++ 240114/일 수 구하기/number-of-days-in-month.java
@@ -7,13 +7,17 @@ public class Main {
         if (n % 2 ==0) {
             if ( n == 2) {
                 System.out.println("28");
-            } else if (n == 8){
+            } else if (n == 8 || n ==10 || n ==12){
                 System.out.println("31");
             } else {
                 System.out.println("30");
             }
         } else {
+            if( n == 9 || n == 11){
+            System.out.println("30");
+            } else {
             System.out.println("31");
+            }
         }
     }
 }
