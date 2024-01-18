4e34583d81d41bbeb64ce4832f41dd3d9598969b
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-01-18 09:40:32 +0900

    Title: n까지 숫자 출력, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240118/n까지 숫자 출력/print-1-to-n.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        int n = sc.nextInt();
+        int i = 1;
+
+        while (i <= n) {
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+}
