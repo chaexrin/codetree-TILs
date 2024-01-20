bf241e294b34c4e65fdcb449e35c138d60ea2dfa
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-01-20 14:39:03 +0900

    Title: 문자 출력하기, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240120/문자 출력하기/print-text.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        char c = sc.next().charAt(0);
+
+        for(int i=1; i<=8; i++) {
+            System.out.print(c);
+        }
+    }
+}
