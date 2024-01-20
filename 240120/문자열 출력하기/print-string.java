dd2e091083510b8a0a61a4c9d92b7f86581ef3c6
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-01-20 14:47:19 +0900

    Title: 문자열 출력하기, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240120/문자열 출력하기/print-string.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++) {
+            System.out.println("LeebrosCode");
+        }
+    }
+}
