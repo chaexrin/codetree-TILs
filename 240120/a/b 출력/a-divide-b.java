5e4f5e55d09000d2f905e898e76c7bf4e4f11ef1
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-01-20 14:54:53 +0900

    Title: a/b 출력, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240120/a/b 출력/a-divide-b.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.printf("%20f", a/b);
+    }
+}
