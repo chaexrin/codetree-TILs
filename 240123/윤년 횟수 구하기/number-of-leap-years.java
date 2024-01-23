1b6fc451c3db36c960a68cc76c8989657ab601a4
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-01-23 21:27:26 +0900

    Title: 윤년 횟수 구하기, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240123/윤년 횟수 구하기/number-of-leap-years.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        int n = sc.nextInt();
+        int y = 0;
+
+        for(int i=1; i<=n; i++){
+            if (i %4 ==0 || i %400 ==0)
+            y++;
+        }
+
+        System.out.print(y-1);
+    }
+}
