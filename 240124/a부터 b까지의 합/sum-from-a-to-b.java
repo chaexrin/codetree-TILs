2b822082efbf34efbbd816e5fb0cb178a26ea41c
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-01-24 10:18:18 +0900

    Title: a부터 b까지의 합, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240124/a부터 b까지의 합/sum-from-a-to-b.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = 0;
+
+        for(int i=a; i <= b; i++) {
+            sum += i;
+        }
+
+        System.out.print(sum);
+    }
+}
