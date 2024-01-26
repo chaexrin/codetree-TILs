cef1d852ef561cc1a9933ac22aa50f240d70f327
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-01-26 12:40:35 +0900

    Title: a의 b승, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240126/a의 b승/a-to-the-power-of-b.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int n = 1;
+
+        for (int i =1; i <= b; i++) {
+            n *= a;
+        }
+        System.out.print(n);
+        
+    }
+}
