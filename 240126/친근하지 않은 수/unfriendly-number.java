ec13a1dcec5d61fb50697e55320f8ae349aeecd5
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-01-26 13:48:17 +0900

    Title: 친근하지 않은 수, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 240126/친근하지 않은 수/unfriendly-number.java
+++ 240126/친근하지 않은 수/unfriendly-number.java
@@ -6,12 +6,15 @@ public class Main {
        int cnt =0;
 
        for (int i=1; i<= n; i++){
-        if(i %2 !=0 || i %3 !=0 || i %5 !=0) {
+        if(i %2 !=0 && i %3 !=0 && i %5 !=0) {
             cnt++;
         }
+        continue;
+      
        }
+          System.out.print(cnt);
 
-        System.out.print(cnt);
+        
 
     }
 }
