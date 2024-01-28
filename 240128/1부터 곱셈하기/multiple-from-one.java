acf2b7d9c17bc30740137bc4522f908369043e44
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-01-28 14:35:35 +0900

    Title: 1부터 곱셈하기, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240128/1부터 곱셈하기/multiple-from-one.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        int n = sc.nextInt();
+        int num = 1;
+
+        for(int i=1; i<=10; i++) {
+            num *= i;
+            if (num >= n) {
+            System.out.print(i);
+            break;
+            }
+           
+        }
+    }
+}
