25574b898e56539a367a205319a50265c8d923db
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-01-29 18:24:07 +0900

    Title: 몫 출력하기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240129/몫 출력하기/print-share.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+       java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        int cnt = 0;
+       while(true) {
+        int n = sc.nextInt();
+         if (n%2==0) {
+                n /= 2;
+                System.out.println(n);
+                cnt++; 
+
+            } else if (n%2==1) {
+                continue;
+                
+            } if (cnt>=3) {
+                break;
+            }
+       }
+    
+    }
+}
