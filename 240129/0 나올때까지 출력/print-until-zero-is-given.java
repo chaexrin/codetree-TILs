ab958ffa0bac6b120c75e9d7376035c61b6e3648
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-01-29 14:04:15 +0900

    Title: 0 나올때까지 출력, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240129/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+       java.util.Scanner sc = new java.util.Scanner(System.in);
+        
+       while(true) {
+        int n = sc.nextInt();
+        if (n == 0) {
+        break;
+       }
+       System.out.println(n);
+       }
+    }
+}
