5c43e04b5d441e9a24c77030a570cbe613d914af
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-01-30 17:06:03 +0900

    Title: 조건에 따른 연산, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240130/조건에 따른 연산/operatino-by-rule.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        while (true) {
+            
+
+            if (n>=1000){
+                System.out.print(cnt);
+                break;
+            }
+
+            if ( n %2 == 0){
+                n = n*3 +1;
+            }
+            else {
+                n = n*2 +2;
+            }
+            cnt++;
+
+        }
+    }
+}
