f8839c1e4a0e15ce71aaccf5f51a0978ed3ae9c2
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-01-31 14:01:56 +0900

    Title: 모두 3의 배수, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

--- 240131/모두 3의 배수/all-multiples-of-3.java
+++ 240131/모두 3의 배수/all-multiples-of-3.java
@@ -2,20 +2,26 @@ public class Main {
     public static void main(String[] args) {
         java.util.Scanner sc = new java.util.Scanner(System.in);
 
-            boolean trois = false;
+            boolean trois = true;
+   int a ;
+    
+        for(int i=1;i<=5;i++){
 
-        for(int i=1; i<=5; i++) {
-            int n = sc.nextInt();
+            a=sc.nextInt();
 
-            if(n %3 == 0) {
-            trois = true;
-            break;
+            if(a%3 != 0) {
+
+                trois = false;
+                break;
+               
             }
+
         }
 
-        if (trois == true)
-        System.out.print(1);
+         if(trois == true)
+            System.out.println("1");
         else
-         System.out.print(0);
+            System.out.println("0");
+
     }
 }
