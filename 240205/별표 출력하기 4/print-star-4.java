693ddaa3f15a01c712dc1399979f66366317ec67
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-02-05 12:55:33 +0900

    Title: 별표 출력하기 4, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240205/별표 출력하기 4/print-star-4.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        int n = sc.nextInt();
+
+
+
+           for (int i =0; i<= n-1; i++){
+            for(int j=n-i; j>=1; j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i=2; i<=n; i++) {
+            for(int j=1; j <= i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
