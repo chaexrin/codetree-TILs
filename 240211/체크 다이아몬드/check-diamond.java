66d2cb4d01d8d4d2d0d84e7d57934f658a131388
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-02-11 20:51:02 +0900

    Title: 체크 다이아몬드, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240211/체크 다이아몬드/check-diamond.java
+++ 240211/체크 다이아몬드/check-diamond.java
@@ -4,24 +4,27 @@ public class Main {
 
         int n = sc.nextInt();
 
-        for (int i=0;i<n;i++){
-            for(int j=0; j<n-i; j++){
+             
+        for(int i=0; i<n; i++) {
+            for(int j=n; j>i+1; j--){
                 System.out.print(" ");
             }
-            for(int k=0; k<i+1;k++){
+            for(int k=0; k<i+1; k++){
                 System.out.print("* ");
             }
             System.out.println();
         }
 
-            for (int i=1;i<n;i++){
-            for(int j=0; j<i+1; j++){
+        for(int i=1; i<n; i++){
+            for(int j=0; j<i; j++){
                 System.out.print(" ");
             }
-            for(int k=n; k>i;k--){
+            for(int k=n; k>i; k--){
                 System.out.print("* ");
             }
             System.out.println();
         }
+         
+
     }
 }
