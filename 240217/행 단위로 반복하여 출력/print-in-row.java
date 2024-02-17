9c2f2b51f3848fff0c482904caed90d06e77c7c5
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-02-17 18:48:11 +0900

    Title: 행 단위로 반복하여 출력, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240217/행 단위로 반복하여 출력/print-in-row.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                System.out.print(j+1);
+            }
+            System.out.println();
+        }
+
+    }
+}
