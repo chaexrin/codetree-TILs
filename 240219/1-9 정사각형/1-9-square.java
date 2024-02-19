a69a19cb5f46a363022b15001883f31a67829b3d
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-02-19 15:30:03 +0900

    Title: 1-9 정사각형, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240219/1-9 정사각형/1-9-square.java
+++ 240219/1-9 정사각형/1-9-square.java
@@ -7,15 +7,11 @@ public class Main {
         
         
         for(int i=0; i<n; i++){
-            while (cnt <10){
             for (int j=0; j<n; j++){
-                 
                 System.out.print(cnt);
                 cnt++;
-                if(cnt == 10){
+                if(cnt == 10)
                     cnt = 1;
-                }
-            }
             }
                 System.out.println();
             }
