ab999b4119a1def1f6eb7fa494e16fd699f55eb2
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-02-29 00:05:10 +0900

    Title: 구구단 만들기, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240229/구구단 만들기/print-multiplication-table.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+       java.util.Scanner sc = new java.util.Scanner(System.in);
+
+       int a = sc.nextInt();
+       int b = sc.nextInt();
+
+       for(int i =1; i<=9; i++){
+        for(int j=b; j>=a; j--){
+            if(j %2==0){
+                System.out.print(j + " * " + i + " = " + j*i + " ");
+                if(j>a){
+                    System.out.print("/ ");
+                }
+            }
+        }
+        System.out.println();
+       }
+       
+    }
+}
