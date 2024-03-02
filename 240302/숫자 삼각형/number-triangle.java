5708085176f295c33bb3d96822ff34b4c2083fd9
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-03-02 19:59:04 +0900

    Title: 숫자 삼각형, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240302/숫자 삼각형/number-triangle.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j< i+1; j++) {
+                System.out.print(j+1 + " ");
+            }
+            System.out.println();
+        }
+    }
+}
