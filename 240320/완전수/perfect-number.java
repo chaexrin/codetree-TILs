83aeeb63dda61148ae4aaac6cef0da49f387d56b
Chaerin <150896440+chaexrin@users.noreply.github.com>
2024-03-20 21:36:45 +0900

    Title: 완전수, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240320/완전수/perfect-number.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        int cnt=0;
+
+        for(int i=start; i<=end; i++){
+            int sum =0;
+            for(int j=1; j<=i-1;j++){
+                
+                if(i %j== 0){
+                    sum += j;
+                }
+            }
+            if(i == sum) {
+                cnt++;
+            }
+            
+        }
+    System.out.print(cnt);
+
+    }
+}
